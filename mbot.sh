fcf50f3dac2b29d507066c2740c1ee429946eeba
rootedcyber93 <108932489+rootedcyber93@users.noreply.github.com>
2022-07-08 23:17:00 +0530

    Add files via upload

--- mbot.sh
+++ mbot.sh
@@ -1,8 +1,1 @@
-git clone https://github.com/rootedcyber93/helios-mirror /maruf
-cd /maruf/helios*
-pwd
-pip install -U -r requirements.txt
-pip3 install --no-cache-dir -r requirements.txt
-
-
-bash start.sh
+git clone https://github.com/rootedcyber93/helios-mirror;cd helios*;pip3 install --no-cache-dir -r requirements.txt;bash start.sh
