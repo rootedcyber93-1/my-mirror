9edda0c5a1ed4d2b2457d59291a9576df0530d5c
rootedcyber93 <108932489+rootedcyber93@users.noreply.github.com>
2022-07-08 21:48:42 +0530

    Add files via upload

---
+++ ultroid.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/rootedcyber93/helios-mirror /nmaruf
+cd /maruf/helios*
+pip install -U -r requirements.txt
+pip3 install --no-cache-dir -r requirements.txt
+pip3 install av --no-binary av
+bash start.sh
