35a3ae635847dd59fe5a923503902d3eeefa0f74
rootedcyber93 <108932489+rootedcyber93@users.noreply.github.com>
2022-07-10 12:43:46 +0530

    Update mbot.sh

--- mbot.sh
+++ mbot.sh
@@ -1,3 +1,4 @@
+cd helios*
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
 docker run --privileged --env-file .env --rm -i ultroid
 git clone https://github.com/rootedcyber93/helios-mirror;cd helios*;pip3 install --no-cache-dir -r requirements.txt;bash start.sh
